5d2e6b63178cf3ec90e58ac483d4964f6da67780
urmanovar <urmanovar@ufntc.ru>
2020-01-29 18:34:24 +0500

    v209: добавил разделение на страницы в списке важных сообщений

--- VkBot/Core/Commands/GetImportant.cs
+++ VkBot/Core/Commands/GetImportant.cs
@@ -35,38 +35,78 @@ namespace VkBot.Core.Commands
             var keyboardButtons = new List<List<MessageKeyboardButton>>();
             using (var con = new Context())
             {
-                foreach (var msg in con.History)
-                {
-                    var parameters = new List<PayloadParam>();
-                    parameters.Add(new PayloadParam()
-                    {
-                        ParamName = "Id сообщения",
-                        Value = msg.Id.ToString(),
-                    });
-                    var payload = new Payload()
-                    {
-                        Params = parameters,
-                    };
-                    var buttons = new List<MessageKeyboardButton>();
-                    buttons.Add(new MessageKeyboardButton()
-                    {
-                        Action = new MessageKeyboardButtonAction() 
-                        { 
-                            Label = $"\"{msg.Comment} \" ({new GetConcreteImportant().KeyWord})",
-                            Type = KeyboardButtonActionType.Text,
-                            Payload = payload.Serialize(),
-                        },
-                        Color = KeyboardButtonColor.Default
-                    });
-                    keyboardButtons.Add(buttons);
-                }
-                
+                var historyList =
+                    from h in con.History
+                    select h;
 
+                var pages = GetMessagePages(historyList.ToList());
+
+                var payload = new Payload().Deserialize(request.Payload);
+                int.TryParse(payload.Params.FirstOrDefault().ToString(), out var pageNumber);
+
+                pageNumber = (pageNumber == 0) ? 1 : pageNumber;
+
+                foreach (var msg in pages[pageNumber - 1])
+                {
+                    keyboardButtons.Add(GetMessageButton(msg));
+                }
             }
             keyboardButtons.AddBackButton();
 
             keyboard.Buttons = keyboardButtons;
             response.Keyboard = keyboard;
+        }
+
+        private List<List<DB.Messages>> GetMessagePages(List<DB.Messages> historyList)
+        {
+            var pages = new List<List<DB.Messages>>();
+            var pageNumber = 0;
+            var buttonsCounter = 0;
+            var page = new List<DB.Messages>();
+            foreach(var item in historyList)
+            {
+                buttonsCounter++;
+                page.Add(item);
+                if (buttonsCounter >= 10)
+                {
+                    buttonsCounter = 0;
+                    pages.Add(page);
+                    page.Clear();
+                }
+            }
+            if (buttonsCounter != 0)
+            {
+                pages.Add(page);
+                page.Clear();
+            }
+
+            return pages;
+        }
+
+        private List<MessageKeyboardButton> GetMessageButton(DB.Messages msg)
+        {
+            var parameters = new List<PayloadParam>();
+            parameters.Add(new PayloadParam()
+            {
+                ParamName = "Id сообщения",
+                Value = msg.Id.ToString(),
+            });
+            var payload = new Payload()
+            {
+                Params = parameters,
+            };
+            var buttons = new List<MessageKeyboardButton>();
+            buttons.Add(new MessageKeyboardButton()
+            {
+                Action = new MessageKeyboardButtonAction()
+                {
+                    Label = $"\"{msg.Comment} \" ({new GetConcreteImportant().KeyWord})",
+                    Type = KeyboardButtonActionType.Text,
+                    Payload = payload.Serialize(),
+                },
+                Color = KeyboardButtonColor.Default
+            });
+            return buttons;
         }
     }
 }
